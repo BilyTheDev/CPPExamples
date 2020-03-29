8021c94e78b5d365bf6ee60eac3c032567e27ea7
BilyTheDev <62808700+BilyTheDev@users.noreply.github.com>
2020-03-29 14:56:25 +0200

    Update Header.h

--- Headers & Classes/Header.h
+++ Headers & Classes/Header.h
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+
+class regularPolygon { 
+public: // Polygon properties: 
+	int sideWidth; 
+	int sideLength;
+	int sidesNumber;
+
+	// function to calculate the area
+
+	int calculateArea(int x, int y)
+	{
+		return x * y;
+	} 
+
+	// function to calculate the perimeter
+
+	int calculatePerimeter(int sideWidthOrLength, int sidesNumber)
+	{
+		return sideWidthOrLength * sidesNumber;
+	} 
+};
